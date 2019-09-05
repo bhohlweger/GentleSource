6422702f901a73570049b2ac03f781b3c4cb2e86
bhohlweger <bernhard.hohlweger@cern.ch>
2019-09-05 16:12:54 +0200

    Expands the Particle for QA and a check of radial expansion as well as boosting

--- Particles/Particle.cxx
+++ Particles/Particle.cxx
@@ -6,31 +6,77 @@
  */
 
 #include "Particle.h"
+#include "TCanvas.h"
 #include <iostream>
 
-Particle::Particle() :
-		fPDGCode(0) {
-	// TODO Auto-generated constructor stub
-
+Particle::Particle(const char* name)
+    : fName(name),
+      fPDGCode(0),
+      fInitHist(false),
+      fAngleSampled(nullptr),
+      fAngleRadial(nullptr) {
 }
 
 Particle::~Particle() {
-	// TODO Auto-generated destructor stub
 }
 
-void Particle::BoostMom(TVector3 boost) {
-	fMom.Boost(-boost.X(), -boost.Y(), -boost.Z());
-	return;
+void Particle::Boost(TVector3 boost) {
+  fMom.Boost(-boost.X(), -boost.Y(), -boost.Z());
+  fPos.Boost(-boost.X(), -boost.Y(), -boost.Z());
+  return;
 }
 
 TVector3 Particle::BoostMomToRestFrame() {
-	float beta = fMom.Beta();
-	TVector3 boost;
-	boost.SetX(beta * cos(fMom.Phi()) * sin(fMom.Theta()));
-	boost.SetY(beta * sin(fMom.Phi()) * sin(fMom.Theta()));
-	boost.SetZ(beta * cos(fMom.Theta()));
+  float beta = fMom.Beta();
+  TVector3 boost;
+  boost.SetX(beta * cos(fMom.Phi()) * sin(fMom.Theta()));
+  boost.SetY(beta * sin(fMom.Phi()) * sin(fMom.Theta()));
+  boost.SetZ(beta * cos(fMom.Theta()));
 
-	fMom.Boost(-boost.X(), -boost.Y(), -boost.Z());
-	return boost;
+  fMom.Boost(-boost.X(), -boost.Y(), -boost.Z());
+  fPos.Boost(-boost.X(), -boost.Y(), -boost.Z());
+  return boost;
+}
+
+bool Particle::RadialExpanding(double cutOff) {
+  if (!fInitHist) {
+    SetupHist();
+  }
+  bool out = true;
+  float angle = fMom.Angle(fPos.Vect());
+  fAngleSampled->Fill(angle);
+  if (TMath::Abs(angle) > cutOff) {
+    out = false;
+  }
+  if (out) {
+    fAngleRadial->Fill(angle);
+  }
+  return out;
+}
+
+void Particle::SetupHist() {
+  fInitHist = true;
+  fAngleSampled = new TH1F(TString::Format("fAngleSampled_%s", fName),
+                           TString::Format("fAngleSampled_%s", fName), 200,
+                           -0.1, TMath::Pi());
+  fAngleRadial = new TH1F(TString::Format("fAngleRadial%s", fName),
+                          TString::Format("fAngleRadial%s", fName), 200, -0.1,
+                          TMath::Pi());
+}
+
+void Particle::DrawQAHisto() {
+  if (!fInitHist) {
+    Warning("Particle::DrawQAHisto",
+            "No Histograms Initialized, not Plotting Angular Distribution \n ");
+    return;
+  }
+  auto cQAAngel = new TCanvas(TString::Format("cQAAngel_%s", fName),
+                              TString::Format("cQAAngel_%s", fName));
+  cQAAngel->Divide(2, 1);
+  cQAAngel->cd(1);
+  fAngleSampled->Draw("");
+  cQAAngel->cd(2);
+  fAngleRadial->Draw("");
+  return;
 }
 
--- Particles/Particle.h
+++ Particles/Particle.h
@@ -10,24 +10,37 @@
 
 #include "TDatabasePDG.h"
 #include "TLorentzVector.h"
+#include "TH1F.h"
+#include <iostream>
 
 class Particle {
 public:
-	Particle();
+	Particle(const char* name);
 	virtual ~Particle();
-	void BoostMom(TVector3 boost);
-	void BoostCoord(TVector3 boost);
+	void Boost(TVector3 boost);
 	TVector3 BoostMomToRestFrame();
 	void SetPDGCode(int PDGCode) {
 		fPDGCode = PDGCode;
 	}
-	void SetParticleMomentum(double px, double py, double pz) {
+	void SetMomentum(double px, double py, double pz) {
 		fMom.SetXYZM(px, py, pz,
 				TDatabasePDG::Instance()->GetParticle(fPDGCode)->Mass());
 	}
+	void SetCoordinates(double x, double y, double z) {
+		fPos.SetXYZM(x,y,z,0); //sets time to assume simultaneous emmission
+	}
+	TLorentzVector GetPos() { return fPos;};
+	TLorentzVector GetMom() { return fMom;};
+	bool RadialExpanding(double cutOff);
+	void SetupHist();
+  void DrawQAHisto();
 private:
+	const char* fName;
 	TLorentzVector fPos;
 	TLorentzVector fMom;
+	bool fInitHist;
+	TH1F* fAngleSampled;
+	TH1F* fAngleRadial;
 	int fPDGCode;
 };
 
